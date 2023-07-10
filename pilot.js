a35084e4cd289499e13b6646759bc2ad1339a7be
ramaalmoamni1 <raalmomani17197@eng.just.edu.jo>
2023-07-10 10:33:50 +0000

    edit the delay

--- pilot.js
+++ pilot.js
@@ -4,32 +4,26 @@ const eventPool = require('./events')
 require('./manager')
 
 
-setTimeout(() => {
-    eventPool.on('new-flight', payload => { 
-      payload.event = 'took-off'; 
-      eventPool.emit('took-off', payload);
-    });
+eventPool.on('new-flight',NewFlight)
 
+function NewFlight(payload){
+  
+  setTimeout(() => {
+    console.log(`Pilot: flight with ID ${payload.Details.flightID} took-off`)
+      payload.event = 'took-off';
+      payload.time = new Date();
+      eventPool.emit('took-off', payload);
   }, 4000);
 
-  eventPool.on('took-off', (payload) => {
-    console.log(`Pilot: flight with ID ${payload.Details.flightID} took-off`)
-    // console.log(payload)
-  })
+  setTimeout(() => {
+    console.log(`Pilot: flight with ID ${payload.Details.flightID} has Arrived`)
+      payload.event = 'Arrived';
+      payload.time = new Date();
+      eventPool.emit('Arrived', payload);
+  }, 7000);
+
+}
 
 
   // setTimeout: The setTimeout function is a built-in JavaScript function that allows you to schedule the execution of a function after a specified delay.
 
-
-setTimeout(() => {
-
-    eventPool.on('new-flight', payload => {
-      payload.event = 'Arrived'; 
-      eventPool.emit('Arrived', payload);
-    });
-  }, 7000);
-
-  eventPool.on('Arrived', (payload) => {
-    console.log(`Pilot: flight with ID ${payload.Details.flightID} has arrived`)
-    // console.log(payload)
-  })
